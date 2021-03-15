6d8abe8ec26e164a6952226393f87a14fd4cfeca
Wojciech Lulek <festorius@gmail.com>
2021-03-15 20:10:18 +0100

    fixed first cycle

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import tsplib95 as tsp
 import matplotlib.pyplot as plt
-import numpy as np
 import networkx as nx
+import numpy as np
 import random
-from typing import List, Set
-from tsplib95.models import StandardProblem
-
 
 def greedy_nn(g: nx.Graph, distance_matrix:np.matrix):
     g.clear_edges()
@@ -16,7 +13,7 @@ def greedy_nn(g: nx.Graph, distance_matrix:np.matrix):
 
     print("Start node:", start_node, "Max distance:", max_distance)
     
-    while graph.number_of_edges() != int(g.number_of_nodes()*0.5):
+    while g.number_of_edges() != int(g.number_of_nodes()*0.5):
         min_distance = np.min(distance_matrix[node][np.nonzero(distance_matrix[node])])
         print("Smallest distance:",min_distance)
         
@@ -33,9 +30,10 @@ def greedy_nn(g: nx.Graph, distance_matrix:np.matrix):
 
 def find_nn(node:int, nodes_list:list, distance_matrix:np.matrix):
     nn= nodes_list[0]
-    min_distance = np.min(distance_matrix[node-1][np.nonzero(distance_matrix[node-1])])
+    min_distance = np.min(distance_matrix[nn-1,node-1])
     for n in nodes_list[1:]:
-        distance = np.min(distance_matrix[node-1][np.nonzero(distance_matrix[node-1])])
+        distance = np.min(distance_matrix[n-1,node-1])
+       
         if distance < min_distance:
             nn = n
             min_distance = distance
@@ -56,7 +54,7 @@ def find_shortest_loop(node,edges,distance_matrix):
 
     return n1_min,n2_min, min_loop
 
-def greedy_cycle(g: nx.Graph, distance_matrix:np.matrix):
+def greedy_cycle(g: nx.Graph, distance_matrix:np.matrix,problem):
     g.clear_edges()
     
     start_node = random.choice(list(g.nodes))
@@ -75,8 +73,8 @@ def greedy_cycle(g: nx.Graph, distance_matrix:np.matrix):
     g.add_edge(node,nearest_neighbor)
     unused_nodes.remove(nearest_neighbor)
     used_nodes.append(nearest_neighbor)
-
     node = nearest_neighbor
+    
     # second edge
     nearest_neighbor = find_nn(node,unused_nodes,distance_matrix)
     g.add_edge(node,nearest_neighbor)
@@ -86,12 +84,14 @@ def greedy_cycle(g: nx.Graph, distance_matrix:np.matrix):
 
     # third edge to start point
     g.add_edge(node,start_node)
+    # g.remove_edge(node,start_node)
     
 
-    while graph.number_of_edges() != int(g.number_of_nodes()*0.5):
+    while g.number_of_edges() != int(g.number_of_nodes()*0.5):
 
         shortest_loop_node = unused_nodes[0]
-        _,_,shortest_loop = find_shortest_loop(unused_nodes[0],g.edges(),distance_matrix)
+        _,_,shortest_loop = find_shortest_loop(shortest_loop_node,g.edges(),distance_matrix)
+        
         for n in unused_nodes[1:]: 
             _,_,loop = find_shortest_loop(n,g.edges(),distance_matrix)
             
@@ -99,16 +99,22 @@ def greedy_cycle(g: nx.Graph, distance_matrix:np.matrix):
                 shortest_loop_node = n
                 shortest_loop = loop
 
-        nn1,nn2,_=find_shortest_loop(shortest_loop_node,g.edges(),distance_matrix)
+        nn1,nn2,_= find_shortest_loop(shortest_loop_node,g.edges(),distance_matrix)
         print("edges_now",g.edges())
         print("new_node",shortest_loop_node,"nn",nn1,nn2)
         
         g.remove_edge(nn1,nn2)
+        
         g.add_edge(shortest_loop_node,nn1)
         g.add_edge(shortest_loop_node,nn2)
 
         used_nodes.append(shortest_loop_node)
         unused_nodes.remove(shortest_loop_node)
+
+        pos = dict(problem.node_coords)
+        nx.draw_networkx(g, pos=pos,font_size=5, node_size=30)
+        plt.savefig("graph.png")
+        input()
     return g
 
 if __name__ == '__main__':
@@ -121,10 +127,10 @@ if __name__ == '__main__':
     np.savetxt('distance_matrix.txt',distance_matrix,fmt='%.2f')
 
     #rozwiązywanie problemu
-    graph = greedy_cycle(graph,distance_matrix)
+    # graph = greedy_nn(graph,distance_matrix)
+    graph = greedy_cycle(graph,distance_matrix,problem)
 
     #rysowanie grafu
     pos = dict(problem.node_coords)
     nx.draw_networkx(graph, pos=pos,font_size=5, node_size=30)
-
     plt.savefig("graph.png")
